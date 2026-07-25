7ce200d8c07832f72e68e366b42ff1dc0dbf45ea
tejaswinidundu1027-afk <tejaswinidundu1027@gmail.com>
2026-07-25 12:53:19 +0530

    Create check kth bit.java

---
+++ check kth bit.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        if(((n >> k) & 1) == 1){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+        
+    }
+}
